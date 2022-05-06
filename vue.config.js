b24d8682c093809031520c54c3864ae6d0639b5d
KangSuji <sukang0608@naver.com>
2022-05-06 12:49:21 +0900

    배포

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  publicPath: '/vue3-news/'
+  publicPath: '/vue3-news/',
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/index/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+    },
+    
+  }
 }
