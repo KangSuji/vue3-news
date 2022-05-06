7ffb32d060f8dbfd17d7d0e81048b5215a146f72
KangSuji <sukang0608@naver.com>
2022-05-06 13:13:58 +0900

    배포

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,6 @@
-module.exports = {
-  pages: {
-    index: {
-      // entry for the page
-      entry: 'src/main.js',
-      // the source template
-      template: 'public/index.html',
-      // output as dist/index.html
-      filename: 'index.html',
-    },
-    
-  }
-}
+module.exports = {    
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue3-test/'
+    : '/',
+   outputDir: 'docs',
+ }
